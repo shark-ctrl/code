54d3fb7c616d8fbe12b36fc6a37a2a7c97f24570
shark-chili <1946955309@qq.com>
2023-07-16 17:36:40 +0800

    提交注释

--- chapter2/sample/main.go
+++ chapter2/sample/main.go
@@ -1,11 +1,14 @@
+// main 函数保存在名为 main 的包里。如果 main 函数不在 main 包里，构建工
+// 具就不会生成可执行的文件
 package main
 
 import (
 	"log"
 	"os"
 
-	_ "github.com/goinaction/code/chapter2/sample/matchers"
-	"github.com/goinaction/code/chapter2/sample/search"
+	//-符号可以保证导入包而不使用，并且可以让matchers的rss.go完成init完成初始化
+	_ "./matchers"
+	"./search"
 )
 
 // init is called prior to main.
--- chapter2/sample/search/search.go
+++ chapter2/sample/search/search.go
@@ -6,27 +6,34 @@ import (
 )
 
 // A map of registered matchers for searching.
+// 以string为key mactcher为value创建一个map
 var matchers = make(map[string]Matcher)
 
 // Run performs the search logic.
 func Run(searchTerm string) {
+	log.Printf("run函数运行了")
 	// Retrieve the list of feeds to search through.
+	//检索要搜索关键字的列表
 	feeds, err := RetrieveFeeds()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// Create an unbuffered channel to receive match results to display.
+	//创建一个没有缓冲区的通到，类型为我们自定义的result类型
 	results := make(chan *Result)
 
 	// Setup a wait group so we can process all the feeds.
+	//设置一个等待数组，等待所有结果的获取
 	var waitGroup sync.WaitGroup
 
 	// Set the number of goroutines we need to wait for while
 	// they process the individual feeds.
+	//等待数组的长度设置为每个数据源的goroutines数量的综合
 	waitGroup.Add(len(feeds))
 
 	// Launch a goroutine for each feed to find the results.
+	// 第一个是索引值，第二个就是jsonList里面的json值了
 	for _, feed := range feeds {
 		// Retrieve a matcher for the search.
 		matcher, exists := matchers[feed.Type]
@@ -35,8 +42,10 @@ func Run(searchTerm string) {
 		}
 
 		// Launch the goroutine to perform the search.
+		//启动goroutine进行搜索工作
 		go func(matcher Matcher, feed *Feed) {
 			Match(matcher, feed, searchTerm, results)
+			//完成后递减waitGroup
 			waitGroup.Done()
 		}(matcher, feed)
 	}
@@ -44,6 +53,7 @@ func Run(searchTerm string) {
 	// Launch a goroutine to monitor when all the work is done.
 	go func() {
 		// Wait for everything to be processed.
+		//设置一个协程等到所有goroutine执行完成
 		waitGroup.Wait()
 
 		// Close the channel to signal to the Display
@@ -53,6 +63,7 @@ func Run(searchTerm string) {
 
 	// Start displaying results as they are available and
 	// return after the final result is displayed.
+	log.Println("展示结果开始")
 	Display(results)
 }
 
