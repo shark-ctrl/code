658c5569db54d943a2b1aae37f57f0fb0aba5457
zhangshiyu <1203783447@qq.com>
2023-07-17 19:19:02 +0800

    单逻辑处理器并发执行协程例子提交

--- chapter6/listing01/listing01.go
+++ chapter6/listing01/listing01.go
@@ -1,5 +1,3 @@
-// This sample program demonstrates how to create goroutines and
-// how the scheduler behaves.
 package main
 
 import (
@@ -8,49 +6,48 @@ import (
 	"sync"
 )
 
-// main is the entry point for all Go programs.
+// main 启动一个逻辑核心，协程会并发(即先后执行)
 func main() {
-	// Allocate 1 logical processor for the scheduler to use.
+	//启动一个逻辑处理器给调度器使用
 	runtime.GOMAXPROCS(1)
 
-	// wg is used to wait for the program to finish.
-	// Add a count of two, one for each goroutine.
+	fmt.Println("启动两个goroutine打印3次英文字母")
+
+	//开启一个计数器，设置为2，等待两个协程执行完,从而做到流程控制
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	fmt.Println("Start Goroutines")
-
-	// Declare an anonymous function and create a goroutine.
+	//启动一个协程打印a-z两次
 	go func() {
-		// Schedule the call to Done to tell main we are done.
+		//协程结束之后，将计数器减1
 		defer wg.Done()
 
-		// Display the alphabet three times
-		for count := 0; count < 3; count++ {
-			for char := 'a'; char < 'a'+26; char++ {
-				fmt.Printf("%c ", char)
+		for i := 0; i < 3; i++ {
+			for i := 'a'; i < 'a'+26; i++ {
+				fmt.Printf("%c ", i)
 			}
+			//换行便于查看
 			fmt.Println("\n")
 		}
 	}()
 
-	// Declare an anonymous function and create a goroutine.
+	//启动一个协程打印A-Z两次
 	go func() {
-		// Schedule the call to Done to tell main we are done.
+		//协程结束之后，将计数器减1
 		defer wg.Done()
 
-		// Display the alphabet three times
-		for count := 0; count < 3; count++ {
-			for char := 'A'; char < 'A'+26; char++ {
-				fmt.Printf("%c ", char)
+		for i := 0; i < 3; i++ {
+			for i := 'A'; i < 'A'+26; i++ {
+				fmt.Printf("%c ", i)
 			}
+			//换行便于查看
 			fmt.Println("\n")
 		}
 	}()
 
-	// Wait for the goroutines to finish.
-	fmt.Println("Waiting To Finish")
+	fmt.Println("等待两个goroutine执行完")
+	//等待两个goroutine执行完
 	wg.Wait()
 
-	fmt.Println("\nTerminating Program")
+	fmt.Println("两个goroutine执行完成")
 }
