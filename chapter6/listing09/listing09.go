b9f62e7d685957c3a5a24ac72b1b12e5376a8ff8
zhangshiyu <1203783447@qq.com>
2023-07-18 09:22:20 +0800

    线程安全问题示例代码提交

--- chapter6/listing09/listing09.go
+++ chapter6/listing09/listing09.go
@@ -1,5 +1,3 @@
-// This sample program demonstrates how to create race
-// conditions in our programs. We don't want to do this.
 package main
 
 import (
@@ -9,43 +7,46 @@ import (
 )
 
 var (
-	// counter is a variable incremented by all goroutines.
 	counter int
-
-	// wg is used to wait for the program to finish.
-	wg sync.WaitGroup
+	wg      sync.WaitGroup
 )
 
-// main is the entry point for all Go programs.
+// main 模拟多线程之间协程操作counter的线程安全问题
 func main() {
-	// Add a count of two, one for each goroutine.
+
+	//设置计数器为协程数2
 	wg.Add(2)
 
-	// Create two goroutines.
+	//启动两个协程对counter进行自增
 	go incCounter(1)
 	go incCounter(2)
 
-	// Wait for the goroutines to finish.
+	//等待两个协程执行完
 	wg.Wait()
-	fmt.Println("Final Counter:", counter)
+
+	fmt.Println("执行完毕,counter:", counter)
+
 }
 
-// incCounter increments the package level counter variable.
 func incCounter(id int) {
-	// Schedule the call to Done to tell main we are done.
+	//函数执行完成后 计数器减1
 	defer wg.Done()
 
-	for count := 0; count < 2; count++ {
-		// Capture the value of Counter.
+	fmt.Println("协程", id, "开始工作")
+
+	for i := 0; i < 2; i++ {
 		value := counter
 
-		// Yield the thread and be placed back in queue.
+		fmt.Println("协程", id, "读取到的counter:", counter)
+		//将协程对应的线程执行权归还，并回到队列中
 		runtime.Gosched()
 
-		// Increment our local value of Counter.
 		value++
 
-		// Store the value back into Counter.
+		fmt.Println("协程", id, "覆盖counter值,value:", value)
 		counter = value
+
+		fmt.Println("协程", id, "完成value自增，自增后的值为", value)
 	}
+
 }
